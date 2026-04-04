b66fe7b936f699f37a606a5ccf746c6fb3c02187
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-05 00:27:14 +1100

    Build a canva

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <canvas width="120" height="120">
+        An alternative text describing what your canvas displays.
+      </canvas>
+    </div>
+  );
+}
+
+export default App;
