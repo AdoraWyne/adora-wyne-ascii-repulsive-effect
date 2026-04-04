eed4db758b5f6031cd1886b2f5789941895ebe4f
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-05 00:37:03 +1100

    useEffect to draw the canva

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
   const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(10, 10, 100, 100);
+  }, []);
+
   return (
     <div>
       <canvas width="120" height="120" ref={canvasRef}>
