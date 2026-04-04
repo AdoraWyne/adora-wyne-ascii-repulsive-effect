c35790c352e3d2721bc808fab8c719fa850796ed
AdoraWyne <dec_kwynez@hotmail.com>
2026-04-05 00:28:28 +1100

    Add useRef to referencing the <canvas> with `canvasRef` so we can add JS to the `<canvas>` later

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
   return (
     <div>
-      <canvas width="120" height="120">
+      <canvas width="120" height="120" ref={canvasRef}>
         An alternative text describing what your canvas displays.
       </canvas>
     </div>
